9674c4dfd1e6cc32dd27f31ba71b493241608593
tons0trees <forrest@fmcclure.com>
2022-10-25 08:00:41 -0600

    tues morning

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank')
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'))
+app.use(express.static('public'))
+
+app.get("/", function(req, res) {
+  const list = postBank.list()
+
+  const htmlString = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>WizardNews</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header>
+          <img src="/logo.png"/>
+          Wizard News
+        </header>
+        ${list.map(elem => `
+          <div class='news-item'>
+            <p>
+              
+            </p>
+          </div>
+        `)}
+      </div>
+      <ul>
+        ${list.map(elem => `<li><p>${elem.title}</p><p>${elem.name}</p></li>`).join(' ')}
+      </ul>
+    </body>
+    </html>
+  `;
+
+  res.send(htmlString);
+
+})
 
 const PORT = 1337;
-
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+
